2078386882c8c11a629b9479b352ce3a0a3156a0
Mostafa Saeidi <msaiedimoien@yahoo.com>
2022-08-06 17:35:08 +0430

    Creating Navbar and Search components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from "./components/Navbar";
 
 const App = () => {
     return (
         <div className="App">
-            <h3>اپلیکیشن مدیریت مخاطبین</h3>
+            <Navbar/>
         </div>
     );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import SearchContact from "./contact/SearchContact";
+
+const Navbar = () => {
+    return(
+        <nav className='navbar navbar-dark navbar-expand-sm shadow-lg'>
+            <div className='container'>
+                <div className='row w-100'>
+                    <div className='col'>
+                        <i className='fa fa-id-badge'/>
+                        وب اپلیکیشن مدیریت {' '}
+                        <span style={{color: 'purple'}}>مخاطبین</span>
+                    </div>
+                    <div className='col'>
+                        <SearchContact/>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
