b8e082a0848c4c0126312e83f3125aef14b3c604
Mostafa Saeidi <msaiedimoien@yahoo.com>
2022-08-06 16:54:06 +0430

    Add a custom font to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
 const App = () => {
-  return (
-    <div className="App">
-      <h3>اپلیکیشن مدیریت مخاطبین</h3>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h3>اپلیکیشن مدیریت مخاطبین</h3>
+        </div>
+    );
 }
 
 export default App;
