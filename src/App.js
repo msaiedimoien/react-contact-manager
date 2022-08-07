05279ca2b0ebc0072a735391f2990d8ce6850d8f
Mostafa Saeidi <msaiedimoien@yahoo.com>
2022-08-07 10:20:15 +0430

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import {useState} from 'react';
 import './App.css';
 import Navbar from "./components/Navbar";
 import Contacts from "./components/contact/Contacts";
 
 const App = () => {
+
+    const [contacts, setContacts] = useState([]);
+
     return (
         <div className="App">
             <Navbar/>
-            <Contacts/>
+            <Contacts contacts={contacts}/>
         </div>
     );
 }
--- src/components/contact/Contacts.jsx
+++ src/components/contact/Contacts.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import {PINK} from "../../helpers/colors";
+import {CURRENTLINE, ORANGE, PINK} from "../../helpers/colors";
 import Contact from "./Contact";
 
-const Contacts = () => {
-    return(
+const Contacts = ({contacts}) => {
+    return (
         <>
             <section className='container'>
                 <div className='grid'>
@@ -22,7 +22,22 @@ const Contacts = () => {
             </section>
             <section className='container'>
                 <div className='row'>
-                    <Contact/>
+                    {
+                        contacts.length > 0
+                            ? contacts.map(c => (
+                                <Contact key={c.id} contact={c}/>
+                            )) :
+                            (
+                                <div className='text-center py-5'
+                                     style={{backgroundColor: CURRENTLINE}}>
+                                    <p className='h3' style={{color: ORANGE}}>
+                                        مخاطب یافت نشد
+                                    </p>
+                                    <img src={require('../../assets/no-found.gif')} alt='یافت نشد' className='w-25'/>
+                                </div>
+                            )
+                    }
+
                 </div>
             </section>
         </>
