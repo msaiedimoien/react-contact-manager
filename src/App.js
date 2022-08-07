51e913a7820bfae6e6b43495b2e11bee593ef1a8
Mostafa Saeidi <msaiedimoien@yahoo.com>
2022-08-07 10:00:34 +0430

    Creating Contacts and Contact components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from "./components/Navbar";
+import Contacts from "./components/contact/Contacts";
 
 const App = () => {
     return (
         <div className="App">
             <Navbar/>
+            <Contacts/>
         </div>
     );
 }
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,63 @@
+import {CURRENTLINE, CYAN, ORANGE, PURPLE, RED} from "../../helpers/colors";
+import React from "react";
+
+const Contact = () => {
+    return(
+        <div className='col-md-6'>
+            <div style={{backgroundColor: CURRENTLINE}}
+                 className='card my-2'>
+                <div className='card-body'>
+                    <div className='row align-items-center d-flex justify-content-around'>
+                        <div className='col-sm-4 col-md-4'>
+                            <img
+                                src="https://via.placeholder.com/150"
+                                alt=""
+                                style={{border: `1px solid ${PURPLE}`}}
+                                className='img-fluid rounded'
+                            />
+                        </div>
+                        <div className='col-md-7 col-sm-7'>
+                            <ul className='list-group'>
+                                <li className='list-group-item list-group-item-dark'>
+                                    نام و نام خانوادگی: {' '}
+                                    <span className='fw-bold'>
+                                                    مصطفی سعیدی
+                                                </span>
+                                </li>
+
+                                <li className='list-group-item list-group-item-dark'>
+                                    شماره موبایل: {' '}
+                                    <span className='fw-bold'>
+                                                    09029002000
+                                                </span>
+                                </li>
+
+                                <li className='list-group-item list-group-item-dark'>
+                                    آدرس ایمیل: {' '}
+                                    <span className='fw-bold'>
+                                                    msaiedimoien@yahoo.com
+                                                </span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className='col-md-1 col-sm-1 d-flex flex-column align-items-center'>
+                            <button className='btn my-1' style={{backgroundColor: ORANGE}}>
+                                <i className='fa fa-eye'/>
+                            </button>
+
+                            <button className='btn my-1' style={{backgroundColor: CYAN}}>
+                                <i className='fa fa-pencil'/>
+                            </button>
+
+                            <button className='btn my-1' style={{backgroundColor: RED}}>
+                                <i className='fa fa-trash'/>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
